6c909006a1be6baac9a6283de5bcabbaf1d44db2
JP <jpdominguez160@gmail.com>
2023-04-16 17:38:03 +0000

    Update 15 files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,53 +1,62 @@
-import Head from 'next/head'
-import { SWRConfig } from 'swr'
-import '../global.css'
-import axios from 'axios'
+import Head from 'next/head';
+import { SWRConfig } from 'swr';
+import '../global.css';
+import axios from 'axios';
 import useSound from 'use-sound';
-import { useState } from 'react';
+import { useState, createContext, useContext } from 'react';
 import Meta from '../components/Meta';
 import Banner from '../components/Banner';
 import Ticker from 'react-ticker';
 
-
-export const AppContext = createContext({})
-export const useAppContext = () => useContext(AppContext as any)
+export const AppContext = createContext({});
+export const useAppContext = () => useContext(AppContext);
 
 export default function MyApp({ Component, pageProps }) {
-    const [play, { stop, isPlaying }] = useSound("/audio/monke-song.mp3");
-    const [audioImage, setAudioImage] = useState('/img/audio-off.png'); // Set initial image to "off" state
-    const handleAudioToggle = () => {
-        if (isPlaying) {
-            stop();
-            setAudioImage('/img/JJuke off.png');
-        } else {
-            play({ loop: true });
-            setAudioImage('/img/Juke on.png');
-        }
-    }
+  const [play, { stop, isPlaying }] = useSound('/audio/monke-song.mp3');
+  const [audioImage, setAudioImage] = useState('/img/audio off.png');
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
 
-    return (
-      <>
-        <Meta />
-        <AppContext.Provider value={{ play }}>
-          <SWRConfig value={{ fetcher: (url) => axios.get(url).then((res) => res.data) }}>
-            <div className="bg-yellow-400 text-black text-xs font-medium p-2">
-             <Ticker>
+  const handleAudioToggle = () => {
+    if (isAudioPlaying) {
+      stop();
+      setIsAudioPlaying(false);
+      setAudioImage('/img/audio off.png');
+    } else {
+      play({ loop: true });
+      setIsAudioPlaying(true);
+      setAudioImage('/img/audio on.png');
+    }
+  };
+
+  return (
+    <>
+      <Meta />
+      <AppContext.Provider value={{ play }}>
+        <SWRConfig value={{ fetcher: (url) => axios.get(url).then((res) => res.data) }}>
+          <div className="bg-yellow-400 text-black text-xs font-medium p-2 relative z-10">
+            <Ticker>
               {({ index }) => (
-                <>
-                  <h1>MINT IN A FEW DAY.{' '}</h1>
-                  <h1><a href="https://discord.gg/cUsHR29meY" className="underline">JOIN DISCORD.</a>{' '}</h1>
-                  <h1>MEET HOT SINGLE MONKE.&nbsp;</h1>
-                  <h1>THIS MINT IS BANANAS.{' '}</h1>
-                  <button onClick={handleAudioToggle}>
-                    <img src={audioImage} alt="Audio" />
-                  </button>
-                </>
+                <div style={{ whiteSpace: 'nowrap' }}>
+                  <h1 style={{ display: 'inline-block' }}>MINT IN A FEW DAYS???.{' '}</h1>
+                  <h1 style={{ display: 'inline-block' }}>
+                    <a href="https://discord.gg/cUsHR29meY" className="underline">
+                      JOIN DISCORD.{' '}
+                    </a>
+                  </h1>
+                  <h1 style={{ display: 'inline-block' }}>MEET HOT SINGLE MONKE.&nbsp;</h1>
+                  <h1 style={{ display: 'inline-block' }}>THIS MINT IS BANANAS.&nbsp;</h1>
+                </div>
               )}
             </Ticker>
-            </div>
-            <Component {...pageProps} />
-          </SWRConfig>
-        </AppContext.Provider>
-      </>
-    );
+          </div>
+          <Component {...pageProps} />
+          <div className="fixed bottom-4 left-4 z-20">
+            <button onClick={handleAudioToggle}>
+              <img src={audioImage} alt="Audio" width="144" height="144" />
+            </button>
+          </div>
+        </SWRConfig>
+      </AppContext.Provider>
+    </>
+  );
 }
