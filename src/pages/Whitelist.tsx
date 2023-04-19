f721ae8931c1a699ed297af2b8283247fd1b58b2
JP <jpdominguez160@gmail.com>
2023-04-19 17:16:05 +0000

    Update 17 files

--- src/pages/Whitelist.tsx
+++ src/pages/Whitelist.tsx
@@ -1,15 +1,22 @@
-iimport Link from 'next/link';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
-export default function Whitelist() {
+export default function About() {
   return (
-    <div className="w-full min-h-screen bg-whitelist bg-cover bg-center">
-      <div className='relative z-10 w-full max-w-screen-2xl mx-auto p-12 py-24 space-y-12 md:space-y-24'>
-        <div className="w-28 md:w-60">
-          <Link href="/home">
-            <img src="/img/button-home.png" alt="" /> 
-          </Link>
+    <div className="fixed h-full w-full opacity-202 bg-cover bg-center" style={{ backgroundImage: 'url("/img/abstract-bg.png")' }}>
+      <div className='relative z-10 w-full max-w-screen-2xl mx-auto flex flex-col md:flex-row items-center h-full p-12 md:px-4'>
+        <div className="w-full md:w-1/3 flex justify-start md:justify-end">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-4">
+            <Link href="/home"><img className='w-full md:w-64' src="/img/button-home.png" alt="" /></Link>
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLScZ0CD8PNf_OOw_-gFj95c0WOYVqJydUNytNU0r8-ESocp2GQ/viewform"><img className='w-full md:w-64' src="/img/Whitelist button.png" alt="" /></Link>
+          </div>
         </div>
-        <img src="/img/join the whitelist.png" alt="" />
+        <div className="w-full md:w-2/3">
+          <img src="/img/join the whitelist.png" alt="" className="w-full" style={{ height: '70%', objectFit: 'contain' }} />
+        </div>
+      </div>
+      <div className='flex justify-center mt-8'>
+        <a href="https://discord.gg/ZRp4QAPF"><motion.img animate={{ rotate: ['1deg', '-1deg', '1deg'] }} transition={{ duration: 4, repeat: Infinity }} src="/img/discord-advt.gif" alt="" /></a>
       </div>
     </div>
   );
